a9e694caed25ea23e28278d6af1b16afeb1cb9c0
SK-928 <k.akirarei928@gmail.com>
2026-06-20 17:57:00 +0900

    chore: biome を設定

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: "specs",
-  
+
   title: "My Awesome Project",
-  description: "A VitePress Site"
-})
+  description: "A VitePress Site",
+});
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
-import Layout from './Layout.vue'
-import type { Theme } from 'vitepress'
-import './style.css'
+
+import type { Theme } from "vitepress";
+import Layout from "./Layout.vue";
+import "./style.css";
 
 export default {
   Layout,
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-} satisfies Theme
-
+    console.log(app, router, siteData);
+  },
+} satisfies Theme;
